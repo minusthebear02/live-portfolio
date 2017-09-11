65caff9cdeddf5492191284bf0d894297980695a
Mark Froehlich <mfroeh2@gmail.com>
2017-09-10 23:11:53 -0400

    Added smooth scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
   $('div.hidden').fadeIn(3000).removeClass('hidden');
-  
+
   $('#contact-form').submit(function() {
     alert("Form submitted, talk to you soon!");
   });
 });
+
+function scroll_to_div(div_id) {
+  $('html,body').animate({
+    scrollTop: $("#"+div_id).offset().top
+  }, 'slow');
+}
